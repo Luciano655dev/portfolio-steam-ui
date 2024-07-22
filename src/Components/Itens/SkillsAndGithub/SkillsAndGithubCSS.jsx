2084542c8eb8cd0d6790756e156f57ae8dd2cafe
Luciano655dev <lucianomenezes655@gmail.com>
2024-07-22 15:19:52 -0400

    retirada navbar e melhoria no Skills and Github

--- src/Components/Itens/SkillsAndGithub/SkillsAndGithubCSS.jsx
+++ src/Components/Itens/SkillsAndGithub/SkillsAndGithubCSS.jsx
@@ -1,143 +1,151 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const SocialMediaContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const SocialMediaImagesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 0.5em;
+
+  margin: 1em;
+
+  div {
     display: flex;
     justify-content: center;
-    flex-direction: row;
+    align-items: center;
 
     background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 0.5em;
 
-    margin: 1em;
+    width: 4em;
+    height: 4em;
+    margin: 0.7em;
 
-    div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        background-color: rgba(0, 0, 0, 0.5);
-
-        width: 4em;
-        height: 4em;
-        margin: 0.7em;
-
-        @media (max-width: 768px) {
-            width: 10vw;
-            height: 10vw;
-            margin: 1vw;
-        }
-
-        &:hover {
-            cursor: pointer;
-        }
-
-        p {
-            color: white;
-        }
+    @media (max-width: 768px) {
+      width: 10vw;
+      height: 10vw;
+      margin: 1vw;
     }
 
-    img{
-        border: 1px solid gray;
-        margin: 0.7em;
-        max-width: 4em;
-        max-height: 4em;
-
-        transition: 0.3s;
-
-        @media (max-width: 768px) {
-            max-width: 10vw;
-            max-height: 10vw;
-            margin: 1vw;
-        }
-
-        &:hover {
-            cursor: pointer;
-            filter: brightness(50%);
-        }
+    &:hover {
+      cursor: pointer;
     }
-`
+
+    p {
+      color: white;
+    }
+  }
+
+  img {
+    border: 1px solid gray;
+    margin: 0.7em;
+    max-width: 4em;
+    max-height: 4em;
+
+    transition: 0.3s;
+
+    @media (max-width: 768px) {
+      max-width: 10vw;
+      max-height: 10vw;
+      margin: 1vw;
+    }
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(50%);
+    }
+  }
+`;
 
 export const GithubInfoContainer = styled.div`
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 
-    margin: 1em;
-    padding: 1em;
+  margin: 1em;
+  padding: 1em;
 
-    background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 0.5em;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 0.5em;
 
-    div{
-        margin-right: 1em;
+  div {
+    margin-right: 1em;
 
-        h1 {
-            font-size: 2em;
-            font-weight: 100;
-            margin-bottom: 0;
-            margin-top: 0;
-            margin-left: 0.5em;
-            color: white;
+    h1 {
+      font-size: 2em;
+      font-weight: 100;
+      margin-bottom: 0;
+      margin-top: 0;
+      margin-left: 0.5em;
+      color: white;
 
-            @media (max-width: 768px) {
-                font-size: 5vw;
-                margin-left: 1vw;
-            }
-        }
-
-        h2{
-            display: flex;
-            flex-direction: row;
-            font-size: 1em;
-            font-weight: 100;
-            margin: 0;
-            margin-left: 1.1em;
-            color: gray;
-
-            a {
-                color: white;
-            }
-
-            p {
-                color: white;
-                text-decoration: underline;
-                margin: 0;
-                margin-left: 0.3em;
-
-                transition: 0.3s;
-                cursor: pointer;
-                &:hover {
-                    color: lightgray;
-                }
-            }
-
-            div {
-                display: none;
-                position: absolute;
-
-                width: 30vw;
-                padding: 1em;
-                transform: translateY(1.2em) translateX(-5em);
-
-                border-radius: 1em;
-                background-color: rgb(0, 0, 0);
-                color: lightgray;
-
-                z-index: 100;
-            }
-
-            &:hover div {
-                display: block;
-            }
-
-            @media (max-width: 768px) {
-                font-size: 2vw;
-                margin-left: 1.1vw;
-            }
-        }
+      @media (max-width: 768px) {
+        font-size: 5vw;
+        margin-left: 1vw;
+      }
     }
-`
+
+    h2 {
+      display: flex;
+      flex-direction: row;
+      font-size: 1em;
+      font-weight: 100;
+      margin: 0;
+      margin-left: 1.1em;
+      color: gray;
+
+      a {
+        color: white;
+      }
+
+      p {
+        color: white;
+        text-decoration: underline;
+        margin: 0;
+        margin-left: 0.3em;
+
+        transition: 0.3s;
+        cursor: pointer;
+        &:hover {
+          color: lightgray;
+        }
+      }
+
+      div {
+        display: none;
+        position: absolute;
+
+        width: 30vw;
+        padding: 1em;
+        transform: translateY(1.2em) translateX(-5em);
+
+        border-radius: 1em;
+        background-color: rgb(0, 0, 0);
+        color: lightgray;
+
+        z-index: 100;
+      }
+
+      &:hover div {
+        display: block;
+      }
+
+      @media (max-width: 768px) {
+        font-size: 2vw;
+        margin-left: 1.1vw;
+      }
+    }
+  }
+`;
+
+export const LangPDiv = styled.div`
+  @media (max-width: 768px) {
+    position: absolute;
+    display: none;
+  }
+`;
--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -1,27 +1,34 @@
-import Navbar from '../Components/Navbar/Navbar'
-import Footer from '../Components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 import {
-    GlobalStyle,
-    StyledBackground,
-    StyledBody,
-    MainContainer
-} from './Home/HomeCSS'
+  GlobalStyle,
+  StyledBackground,
+  StyledBody,
+  MainContainer,
+} from "./Home/HomeCSS";
 
-export default function DefaultPage(){
-    const backgroundSrc = 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/items/1502190/ab689603a427930cb69d5ad4db5c255ccd334133.mp4'
+export default function DefaultPage() {
+  const backgroundSrc =
+    "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/items/1502190/ab689603a427930cb69d5ad4db5c255ccd334133.mp4";
 
-    return <div style={{ overflow: 'hidden' }}>
-        <GlobalStyle></GlobalStyle>
-        <StyledBackground autoPlay muted loop src={backgroundSrc} type="video/mp4" />
+  return (
+    <div style={{ overflow: "hidden" }}>
+      <GlobalStyle></GlobalStyle>
+      <StyledBackground
+        autoPlay
+        muted
+        loop
+        src={backgroundSrc}
+        type="video/mp4"
+      />
 
-        <Navbar></Navbar>
-        
-        <StyledBody>
-            <Outlet></Outlet>
-        </StyledBody>
+      <StyledBody>
+        <Outlet></Outlet>
+      </StyledBody>
 
-        <Footer></Footer>
+      <Footer></Footer>
     </div>
+  );
 }
